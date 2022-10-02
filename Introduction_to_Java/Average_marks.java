f0cd16ba8c46855ae99208b8e97394040ba50507
Prince <90469232+Prince-0000@users.noreply.github.com>
2022-10-02 22:42:43 +0530

    Create Average_marks.java

---
+++ Introduction_to_Java/Average_marks.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Average_marks {
+	public static void main(String[] args) {
+    
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the marks of top three subjects : ");
+        int m1 = s.nextInt();
+        int m2 = s.nextInt();
+        int m3 = s.nextInt();
+        System.out.println((m1+m2+m3)/3);
+	}
+}
